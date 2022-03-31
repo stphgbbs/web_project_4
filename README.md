d9abfa24153d78fdf273799a8d50f3f3d78c1895
stephgibbs <stephanygibbs03@gmail.com>
2022-03-31 17:43:22 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Project 4: Around The U.S. by Stephany Gibbs
+
+Website documenting pictures of destinations around the U.S.
+
+- Figma
+- Images
+- CSS
+- Responsive design
+- Javascript
+
+Link to Project Deployed on GitHub Pages
---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const popup = document.querySelector(".popup");
+const profileName = document.querySelector(".profile__title");
+const editButton = document.querySelector(".profile__buttons-edit");
+const aboutMe = document.querySelector(".profile__subtitle");
+const modalWindow = document.querySelector(".popup__form");
+const closeButton = document.querySelector(".popup__form-close");
+const saveButton = document.querySelector(".popup__form-button");
+const heartIcons = document.querySelectorAll(".card__heart-icon");
+const name = document.querySelector(".popup__input-name");
+const aboutInfo = document.querySelector(".popup__input-description");
+
+const popUp = function () {
+  name.value = profileName.textContent;
+  aboutInfo.value = aboutMe.textContent;
+  popup.style.visibility = "visible";
+};
+
+const closePopup = function () {
+  popup.style.visibility = "hidden";
+};
+
+const updateProfile = function () {
+  event.preventDefault();
+  profileName.textContent = name.value;
+  aboutMe.textContent = aboutInfo.value;
+  closePopup();
+};
+
+const heartIconsUpdate = function () {
+  this.classList.toggle("card__heart-icon_active");
+  console.log("hi");
+};
+
+editButton.addEventListener("click", popUp);
+
+closeButton.addEventListener("click", closePopup);
+
+modalWindow.addEventListener("submit", updateProfile);
+
+heartIcons.forEach(function (button) {
+  button.addEventListener("click", function onClick() {
+    button.classList.toggle("card__heart-icon_active");
+    button.classList.toggle("card__heart-icon");
+  });
+});
