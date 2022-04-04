350e14e4bd5500a336139170f94491abd67b0ac9
stephgibbs <stephanygibbs03@gmail.com>
2022-04-04 00:37:32 -0500

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -9,37 +9,37 @@ const heartIcons = document.querySelectorAll(".card__heart-icon");
 const name = document.querySelector(".popup__input-name");
 const aboutInfo = document.querySelector(".popup__input-description");
 
-const popUp = function () {
-  name.value = profileName.textContent;
-  aboutInfo.value = aboutMe.textContent;
-  popup.style.visibility = "visible";
-};
-
-const closePopup = function () {
-  popup.style.visibility = "hidden";
-};
-
-const updateProfile = function () {
-  event.preventDefault();
-  profileName.textContent = name.value;
-  aboutMe.textContent = aboutInfo.value;
-  closePopup();
-};
-
 const heartIconsUpdate = function () {
   this.classList.toggle("card__heart-icon_active");
   console.log("hi");
 };
-
-editButton.addEventListener("click", popUp);
-
-closeButton.addEventListener("click", closePopup);
-
-modalWindow.addEventListener("submit", updateProfile);
 
 heartIcons.forEach(function (button) {
   button.addEventListener("click", function onClick() {
     button.classList.toggle("card__heart-icon_active");
     button.classList.toggle("card__heart-icon");
   });
+
+  const popUp = function () {
+    name.value = profileName.textContent;
+    aboutInfo.value = aboutMe.textContent;
+    popup.style.visibility = "visible";
+  };
+
+  const closePopup = function () {
+    popup.style.visibility = "hidden";
+  };
+
+  const updateProfile = function () {
+    event.preventDefault();
+    profileName.textContent = name.value;
+    aboutMe.textContent = aboutInfo.value;
+    closePopup();
+  };
+
+  editButton.addEventListener("click", popUp);
+
+  closeButton.addEventListener("click", closePopup);
+
+  modalWindow.addEventListener("submit", updateProfile);
 });
