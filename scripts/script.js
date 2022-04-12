d0e2b0521ca9bee927d2d6f6877e114cee0ca8f9
stephgibbs <stephanygibbs03@gmail.com>
2022-04-11 23:41:31 -0500

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,32 @@
 const popup = document.querySelector(".popup");
 const profileName = document.querySelector(".profile__title");
 const editButton = document.querySelector(".profile__buttons-edit");
-const aboutMe = document.querySelector(".profile__subtitle");
+const subtitle = document.querySelector(".profile__subtitle");
 const modalWindow = document.querySelector(".popup__form");
 const closeButton = document.querySelector(".popup__form-close");
 const saveButton = document.querySelector(".popup__form-button");
-const heartIcons = document.querySelectorAll(".card__heart-icon");
-const name = document.querySelector(".popup__input-name");
-const aboutInfo = document.querySelector(".popup__input-description");
+const name = document.querySelector("#name-input");
+const descriptionInputEL = document.querySelector("#description-input");
 
-const heartIconsUpdate = function () {
-  this.classList.toggle("card__heart-icon_active");
+const popUp = function () {
+  name.value = profileName.textContent;
+  descriptionInputEL.value = subtitle.textContent;
+  popup.classList.add("popup_visible");
 };
 
-heartIcons.forEach(function (button) {
-  button.addEventListener("click", function onClick() {
-    button.classList.toggle("card__heart-icon_active");
-    button.classList.toggle("card__heart-icon");
-  });
+const closePopup = function () {
+  popup.classList.remove("popup_visible");
+};
 
-  const popUp = function () {
-    name.value = profileName.textContent;
-    aboutInfo.value = aboutMe.textContent;
-    popup.style.visibility = "visible";
-  };
+const updateProfile = function (event) {
+  event.preventDefault();
+  profileName.textContent = name.value;
+  subtitle.textContent = descriptionInputEL.value;
+  closePopup();
+};
 
-  const closePopup = function () {
-    popup.style.visibility = "hidden";
-  };
+editButton.addEventListener("click", popUp);
 
-  const updateProfile = function () {
-    event.preventDefault();
-    profileName.textContent = name.value;
-    aboutMe.textContent = aboutInfo.value;
-    closePopup();
-  };
+closeButton.addEventListener("click", closePopup);
 
-  editButton.addEventListener("click", popUp);
-
-  closeButton.addEventListener("click", closePopup);
-
-  modalWindow.addEventListener("submit", updateProfile);
-});
+modalWindow.addEventListener("submit", updateProfile);
